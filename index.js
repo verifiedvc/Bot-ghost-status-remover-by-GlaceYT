22052b0866a8b0a249d5a7b4da0c5b17c21af4bc
verifiedvc <vcverifiedroblox@gmail.com>
2024-08-24 11:31:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Watching over Storms Vouches"];
 
 
 let currentIndex = 0;
